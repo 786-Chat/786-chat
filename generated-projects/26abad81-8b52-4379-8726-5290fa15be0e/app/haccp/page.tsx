e3a16feb7cced79268860842fdb508c22dfbc8e7
786.Chat <link24.7days@gmail.com>
2026-08-28 22:22:31 +0100

    build(786.chat): publish generated project New AI Project

---
+++ generated-projects/26abad81-8b52-4379-8726-5290fa15be0e/app/haccp/page.tsx
@@ -0,0 +1,11 @@
+import { HaccpView } from "@/components/haccp-view";
+import { MobileBackButton } from "@/components/mobile-back-button";
+
+export default function HaccpPage() {
+  return (
+    <>
+      <MobileBackButton />
+      <HaccpView />
+    </>
+  );
+}
